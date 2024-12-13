dedf901d6ee78b8b424bf6756a0af190a00c67eb
habib-hb <developerhabib1230@gmail.com>
2024-12-13 14:40:40 +0600

    f

--- application/views/footer.php
+++ application/views/footer.php
@@ -3,7 +3,7 @@
 </div>
 <footer class="" style="clear: both;">
 	<div class="container-fluid">
-		<p>&copy; 2023 | All Rights Reserverd <br> Point Of Sales And Inventory Management System</p>
+		<p>&copy; 2024 | All Rights Reserverd <br> Point Of Sales And Inventory Management System</p>
 	</div>
 </footer>
 
